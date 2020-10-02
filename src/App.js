1e1e4a73547843f4fc390a7738a53f04983fb870
Daniel Mateos <dmateosc@outlook.es>
2020-10-02 06:30:26 +0000

    Changing some silly things

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "./components/layout"
 import NoteList from "./components/noteList"
 
-function App() {
+const App = () => {
   return (
     <Layout>
       <NoteList />
